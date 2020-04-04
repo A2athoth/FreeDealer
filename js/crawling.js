afc165a8510a09ae8bf996277d7a46ded503044a
j9uery <j9uery@gmail.com>
2020-04-05 03:51:27 +0900

    19세 이상 동의 표하는 게임 경우 테스트중

--- js/crawling.js
+++ js/crawling.js
@@ -1,10 +1,15 @@
 // 본격적인 내용 크롤링
 
+// 19세 겜같이 동의를 요하는 페이지들 뚫는 법을 찾아야함
+//
+// https://blog.scrapinghub.com/2017/07/07/scraping-the-steam-game-store-with-scrapy
+//     https://github.com/aesuli/steam-crawler/blob/master/steam-game-crawler.py
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
 let _lang;
-let _url = 'https://store.steampowered.com/app/546560/HalfLife_Alyx/';
+let _url = 'https://store.steampowered.com/app/12210/Grand_Theft_Auto_IV_The_Complete_Edition/';
 if (_url.charAt(_url.length-1) === '/'){_url = _url.slice(0,-1);}   // 경로 끝에 /가 있으면 떼기
 
 // _lang = '/?l=english';
@@ -13,8 +18,7 @@ _lang = '/?l=koreana';
 
 const getHtml = async () => {
     try {
-        return await axios.get(
-            _url+_lang);
+        return await axios.get(_url+_lang, {headers:{':authority': 'steamcdn-a.akamaihd.net'}});
     } catch (error) {
         console.error(error);
     }
