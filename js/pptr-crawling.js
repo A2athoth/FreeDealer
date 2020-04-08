8c2ab92e48df8f7745935b63cdf75dd38114f814
j9uery <j9uery@gmail.com>
2020-04-09 01:09:39 +0900

    출력 양식 좀 수정, etc

--- js/pptr-crawling.js
+++ js/pptr-crawling.js
@@ -1,6 +1,5 @@
-// 나이제한을 지나가면 언어 query param이 풀린다. 한국어를 클릭하는 이벤트쪽을 함수를 쓰든 퍼펫티어 이벤트를 하든 해야할듯... 그럼 애초에 쿼리파람도 필요없었군...
-// 초반 언어 분기를 두고 if나 for문으로 구성해야 할듯
-// const _target = "https://store.steampowered.com/app/1085660/Destiny_Guardians/";
+// 해야할것 : 오류날 여지 체크 후 오류나면 어떻게 할지
+
 const _target = "https://store.steampowered.com/app/12210/Grand_Theft_Auto_IV_The_Complete_Edition/";
 // const _target = "https://store.steampowered.com/app/1085660/Destiny_Guardians/";
 // const _target = "https://store.steampowered.com/app/952060/RESIDENT_EVIL_3/";
@@ -43,21 +42,21 @@ puppeteer.launch({
 
     const title = await page.waitFor( "div.apphub_AppName" );
     const titleTxt = await page.evaluate( e => e.textContent, title );
-    console.log("-. 게임제목 : ", titleTxt);
+    console.log("- 게임제목 :", titleTxt);
 
     const desc = await page.waitFor( "div.game_description_snippet" );
     const descTxt = await page.evaluate( e => e.textContent, desc );
-    console.log("-. 게임설명 : ", descTxt);
+    console.log("- 게임설명 :", descTxt.trim());    // trim메소드로 앞뒤 공백 제거
 
     // 두번째 저 클래스를 가진 애가 있으면 발동
     if (await page.$("div.user_reviews_summary_row:nth-child(2)") !== null) {
         const recentReview = await page.waitFor( "span.game_review_summary:first-child" );
         const recentReviewTxt = await page.evaluate( e => e.textContent, recentReview );
-        console.log("-. 최근평가 : ", recentReviewTxt);
+        console.log("- 최근평가 :", recentReviewTxt);
     }
     const totalReview = await page.waitFor( "span.game_review_summary[itemprop='description']" );
     const totalReviewTxt = await page.evaluate( e => e.textContent, totalReview );
-    console.log("-. 전체평가 : ", totalReviewTxt);
+    console.log("- 전체평가 :", totalReviewTxt);
 
     await delay(100);
     await browser.close();
